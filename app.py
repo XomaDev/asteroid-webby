90236a01eac04ee858fe31f1130765338e35fa38
XomaDev <bgpsdr@gmail.com>
2021-05-12 18:33:51 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+import chocolateo
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Asteroid is working!'
+
+
+@app.route('/api/answer')
+def api():
+    text = request.args.get("text")
+    result = chocolateo.web_scrape(text)
+    return '{ "answer":"' + result[0] + '", "source":"' + result[1] + '"}'

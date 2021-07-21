bdf3038c595cc6de56bc87abe32593c34f204f34
Kumaraswamy B G <71964026+XomaDev@users.noreply.github.com>
2021-07-21 19:04:17 +0530

    Update index.py

--- index.py
+++ index.py
@@ -24,26 +24,3 @@ def answer():
         status=200,
         mimetype='application/json'
     )
-
-
-@app.route('/api/info')
-def info():
-    return app.response_class(
-        response=JSONEncoder().encode({
-            "answer": chocolateo.bingScrape(request.args.get('text')),
-        }),
-        status=200,
-        mimetype='application/json'
-    )
-
-
-@app.route('/api/command/')
-def commandScrape():
-    return app.response_class(
-        response=JSONEncoder().encode({
-            "result": commandscrape.command_scrape(request.args.get('text')),
-        }),
-        status=200,
-        mimetype='application/json'
-    )
-

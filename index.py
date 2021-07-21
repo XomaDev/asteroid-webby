bb79277f746149794af12d2c815193c59792f309
Kumaraswamy B G <71964026+XomaDev@users.noreply.github.com>
2021-07-21 19:01:32 +0530

    Update index.py

--- index.py
+++ index.py
@@ -13,13 +13,35 @@ def home():
 
 
 @app.route('/api/answer')
-def api():
+def answer():
     result = chocolateo.web_scrape(request.args.get("text"))
 
     return app.response_class(
         response=JSONEncoder().encode({
             "answer": result[0],
             "source": result[1]
+        }),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/api/info')
+def info():
+    return app.response_class(
+        response=JSONEncoder().encode({
+            "answer": chocolateo.bingScrape(request.args.get('text')),
+        }),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/api/command/')
+def commandScrape():
+    return app.response_class(
+        response=JSONEncoder().encode({
+            "result": commandscrape.command_scrape(request.args.get('text')),
         }),
         status=200,
         mimetype='application/json'
